ed6c43e181d46baeafefed725850c118622bba44
putao520 <yuyao1022@hotmail.com>
2022-02-20 16:30:04 +0800

    修复反射时包装/拆箱类型未归一BUG

--- src/common/java/Reflect/MethodAccess.java
+++ src/common/java/Reflect/MethodAccess.java
@@ -7,11 +7,21 @@ import java.util.HashMap;
 import java.util.List;
 
 public class MethodAccess {
-    private final Class<?> cls;
+    private static final Class<?>[][] sameClassTemplate = new Class<?>[][]{
+            {Integer.class, int.class},
+            {Long.class, long.class},
+            {Float.class, float.class},
+            {Double.class, double.class},
+            {Boolean.class, boolean.class},
+            {Byte.class, byte.class},
+            {Character.class, char.class},
+            {Short.class, short.class},
+    };
+    // private final Class<?> cls;
     private final HashMap<String, List<MethodMap>> clsMethodMap = new HashMap<>();
 
     private MethodAccess(Class<?> cls) {
-        this.cls = cls;
+        // this.cls = cls;
         for (Method m : cls.getMethods()) {
             var arr = clsMethodMap.getOrDefault(m.getName(), new ArrayList<>());
             arr.add(new MethodMap(m.getParameterTypes(), m));
@@ -21,6 +31,33 @@ public class MethodAccess {
 
     public static MethodAccess get(Class<?> cls) {
         return new MethodAccess(cls);
+    }
+
+    // 比较输入参数类型 与 接口定义类型
+    // 强制将 int/integer 等包装类型转换为原始类型
+    public boolean parameterCompare(Class<?> declared, Class<?> actual) {
+        // 从 sameClassTemplate 拿到符合 declared 类型对
+        Class<?>[] classList = null;
+        for (int i = 0, l = sameClassTemplate.length; i < l && classList == null; i++) {
+            var arr = sameClassTemplate[i];
+            for (var c : arr) {
+                if (c.equals(declared)) {
+                    classList = arr;
+                    break;
+                }
+            }
+        }
+        // 如果没有找到符合的类型对
+        if (classList == null) {
+            return declared.equals(actual);
+        }
+        // 如果找到了符合的类型对
+        for (var c : classList) {
+            if (c.equals(actual)) {
+                return true;
+            }
+        }
+        return false;
     }
 
     public Object invoke(Object ins, String functionName, Object... Parameters) {
@@ -41,16 +78,13 @@ public class MethodAccess {
             if (l > 0) {
                 int _nearIdx = -1;
                 for (int i = 0; i < l; i++) {
-                    // 输入参数为空，但是列表有数据，直接退出
-                    if (Parameters == null) {
-                        break;
-                    }
                     // 空参数
                     if (Parameters[i] == null) {
                         continue;
                     }
                     // 任意类型 或者 类型不正确
-                    if (!pa[i].equals(Object.class) && !pa[i].equals(Parameters[i].getClass())) {
+                    // if (!pa[i].equals(Object.class) && !pa[i].equals(Parameters[i].getClass())) {
+                    if (!pa[i].equals(Object.class) && !parameterCompare(pa[i], Parameters[i].getClass())) {
                         continue;
                     }
                     // 获得参数列表最接近的
@@ -77,9 +111,7 @@ public class MethodAccess {
 
         try {
             return nearMethod.invoke(ins, Parameters);
-        } catch (IllegalAccessException e) {
-            e.printStackTrace();
-        } catch (InvocationTargetException e) {
+        } catch (IllegalAccessException | InvocationTargetException e) {
             e.printStackTrace();
         }
         return null;
